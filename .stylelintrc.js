540a6fc7988667362d33462f60169c7feccaccc4
Jacky-Summer <1106619871@qq.com>
2021-03-19 22:57:28 +0800

    chore: add plugins rules to stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
     'no-descending-specificity': null, // 禁止低优先级的选择器出现在高优先级的选择器之后
     'declaration-empty-line-before': 'never', // 要求或禁止在声明语句之前有空行
